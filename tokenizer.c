13c406d982cb5c060add5d0aaf6b4db540eef9b6
Admunoz8 <60449029+Admunoz8@users.noreply.github.com>
2021-10-10 13:40:24 -0600

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,192 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#include <stdlib.h> // malloc & free
-#include <stdint.h> // use guaranteed 64-bit integers
-#include "tokenizer.h" // Create header file and reference that
-#include "memory.h" // built-in functions to read and write to a specific file
 
-int32_t* reg; // Array of 32 32-bit registers
-
-void init_regs();
-bool interpret(char* instr);
-void write_read_demo();
-bool areSame(char* str1, char* str2);
-int caseHandler(char* str);
-int findRegNum(char* str);
-void print_regs();
-
-void print_regs(){
-  int col_size = 10;
-  for (int i = 0; i < 8; i++){
-    printf("X%02i:%.*lld", i, col_size, (long long int) reg[i]);
-    printf("X%02i:%.*lld", i+8, col_size, (long long int) reg[i+8]);
-    printf("X%02i:%.*lld", i+16, col_size, (long long int) reg[i+16]);
-    printf("X%02i:%.*lld", i+24, col_size, (long long int) reg[i+24]);
-  }
-}
-
-/*Receives register format strings and return the int value of the register.
-For example, "X13" -> 13*/
-int findRegNum(char* str){ 
-  int count = 0;
-  char num[2];
-  while(*(str+count)!='\0'){
-    count++;
-  }
-  if (count >= 3){ //accounting for the last token to have an extra NULL at the end.
-    num[0] = *(str+1); num[1] = *(str+2);
-  }
-  else
-    num[0] = *(str+1);
-
-  return  atoi(num);
-}
-//this will be able to handle the strings as a int so we can use them  in our swithc stateement 
-int caseHandler(char* str){
-  if (areSame(str, "LW")) {return 0;}
-  else if (areSame(str, "SW")) {return 1;}
-  else if (areSame(str, "ADD")) {return 2;}
-  else if (areSame(str, "ADDI")) {return 3;}
-  else if (areSame(str, "AND")) {return 4;}
-  else if (areSame(str, "OR")) {return 5;}
-  else if (areSame(str, "XOR")) {return 6;}
-  else
-    return -1;
-}
-//this meethod helps my case handler help by letting me compare th given string
-bool areSame(char* str1, char* str2){
-  char* temp1 = str1;
-  char* temp2 = str2;
-  int count1 = 0;
-  int count2 = 0;
-
-  while (*(temp1+count1) != '\0'){
-    count1++;
-  }
-  while (*(temp2+count2) != '\0'){
-    count2++;
-  }
-  if (count1 != count2){return false;}
-  
-  int i = 0;
-  while (*(str1+i) != '\0' && *(str2+i) != '\0'){
-    if (*(str1+i) != *(str2+i))
-      return false;
-    i++;
-  }
-  return true;
-}
-
-/**
- * Initialize register array for usage.
- * Malloc space for each 32-bit register then initializes each register to 0.
- * Do not alter this function!
+/* Return true (non-zero) if c is a whitespace character
+   ('\t', ' ').
+   Zero terminators are not printable (therefor false).
  */
-void init_regs(){
-	int reg_amount = 32;
-	reg = malloc(reg_amount * sizeof(int32_t)); // 32 * 4 bytes
-	for(int i = 0; i < 32; i++)
-		reg[i] = i;
-}
-
-
-
-/**
- * Fill out this function and use it to read interpret user input to execute RV64 instructions.
- * You may expect that a single, properly formatted RISC-V instruction string will be passed
- * as a parameter to this function.
- */
- //this method is use so that we can properly interpert, it will use pointers 
-bool interpret(char* instr){
-  char* textFile = "mem.txt";
-  char **pointerS;
-  pointerS = tokenize(instr, ' ');
-  //print_all_tokens(pointerStorage); use for debugging purposes.
-  int caseNum = caseHandler(pointerS[0]); // decides what case the instruction is.
-  switch(caseNum){
-    //this case is for LW,The lw instruction loads a word into a register from memory.
-  case 0:
-    printf("LW read");
-    char* saveToLW = *(pointerS+1);
-    int saveToLWint = findRegNum(saveToLW);
-    char** memoryTokensLW = tokenize(*(pointerS+2), '('); // splits the main 3rd token in 2.
-    char** cleanMemoryLW = tokenize(*(memoryTokensLW+1),')'); // cleans the mem token to only reg.
-
-    char* memoryRegLW = *cleanMemoryLW;
-    int memoryRegLWint = findRegNum(memoryRegLW);
-
-    int32_t addressLW = atoi(*memoryTokensLW) + memoryRegLWint;
-    int32_t readLW = read_address(addressLW, textFile);
-    reg[saveToLWint] = readLW; // register is given whatever was read in addressLW
-    break;
-    //this case is for SW,it will copy the data from a register to memory. The register is not changed. 
-
-  case 1:
-    printf("SW read");
-    char* saveFromSW = *(pointerS+1);
-    int saveFromSWint = findRegNum(saveFromSW);
-    char** memoryTokensSW = tokenize(*(pointerS+2), '(');
-    char** cleanMemorySW = tokenize(*(memoryTokensSW+1), ')');
-    char* memoryRegSW = *cleanMemorySW;
-    int memoryRegSWint = findRegNum(memoryRegSW);
-    
-    int32_t data_to_writeSW = reg[saveFromSWint];
-    int32_t addressSW = atoi(*memoryTokensSW) + memoryRegSWint; 
-    int32_t write = write_address(data_to_writeSW, addressSW, textFile);
-    
-    break;
-    //Adds the first operand (destination operand) and the second operand (source operand) and stores the result in the destination operand.
-  case 2:
-    printf("ADD read");
-    char* saveTo = *(pointerS+1);
-    char* firstOperand = *(pointerS+2);
-    char* secondOperand = *(pointerS+3);
-
-    int saveToReg = findRegNum(saveTo);
-    int firstReg = findRegNum(firstOperand);
-    int secReg = findRegNum(secondOperand);
-
-    reg[saveToReg] = reg[firstReg] + reg[secReg];
-    printf("%d", reg[saveToReg]);
-    break;
-    //This case is for ADDI instruction. Addi is used to  adds the immediate specified in the instruction to a value in a register
-  case 3:
-    printf("ADDI read");
-    char* saveToADDI = *(pointerS+1);
-    char* addTo = *(pointerS+2);
-    int add = atoi(*(pointerS+3));
-
-    int saveToADDIint = findRegNum(saveToADDI);
-    int addToint = findRegNum(addTo);
-
-    reg[saveToADDIint] = reg[addToint] + add;
-    break;
-  default:
-    printf("ERROR");
+bool delim_character (char c, const char delim){
+  if (c == '\t' || c == delim)
+    return true;
+  else
     return false;
-  }
-  return true;
 }
 
-/**
- * Your code goes in the main
- *
- */
-int main(){
-	// Do not write any code between init_regs
-	init_regs(); // DO NOT REMOVE THIS LINE
+/* Return true (non-zero) if c is a non-whitespace character
+   (not tab or space). Zero terminators are not printable
+   (therefore false).
+*/
+bool non_delim_character(char c, const char delim){
+  if (c == '\t' || c == '\0' || c == delim)
+    return false;
+  else
+    return true;
+}
 
-	char input[1000]; // User input can only be 100 chars long.
-	printf("Enter 'x' to quit.\n");
-  //while loop so it can do the functions over and oveer until user press x, therefore it breaks
-	while(1){
-	  printf("Input: ");
-	  fgets(input, sizeof(input), stdin);
-	  if (*input == 'x'){break;}
-	  interpret(input);
-	  printf("\n");
-	}
-	print_regs();
+/* Returns a pointer to the first character of the next
+   space-separated word.
+*/
+char *word_start(char* str, const char delim){
+  while(delim_character(*str, delim))
+    str++;
+  return str;
+}
 
-	return 0;
+/* Returns a pointer to the first space character of the zero
+   terminated string.
+*/
+char *end_word(char* str, const char delim){
+  while(non_delim_character(*str, delim))
+    str++;
+  return str;
+}
+
+/* counts the number of words or tokens*/
+int count_tokens(char* str, const char delim){
+  int count = 0;
+  while(*str != '\0'){
+    if (delim_character(*str, delim))
+      count++;
+    str++; // moves pointer along the str array.
+  }
+  count++; // accounts for the terminator.
+  return count;
+}
+
+/* Returns a freshly allocated zero-terminated vector of freshly 
+   allocated space-seperated tokens from zero-terminated str.
+   For example, tokenize("hello world string") would result in:
+     tokens[0] = "hello"
+     tokens[1] = "world"
+     tokens[2] = "string"
+     tokens[3] = 0
+*/
+char *copy_str(char *inStr, short len){
+  /*Allocates enough memory for an array of chars the size of inStr*/
+  char *copy = (char*)malloc((len)*sizeof(char)); 
+  for (int i = 0; i < len; i++)
+    *(copy+i) = *(inStr+i);
+  *(copy+len) = '\0';
+  return copy;
+}
+
+char** tokenize(char* str, const char delim){
+  int count = count_tokens(str, delim);
+  /*Allocates enough memory for an array containing pointers to other arrays.*/
+  char **arrayOfPointers = (char**)malloc((count+1)*sizeof(char*));
+  char *end;
+  char *start = word_start(str, delim);
+  int i;
+  for (i = 0; i < count; i++){
+    end = end_word(start, delim);
+    arrayOfPointers[i] = copy_str(start, (end-start)); //end-start finds the offset for the token
+    start = word_start(end, delim);
+  }
+  arrayOfPointers[i] = NULL; // makes the last item in arrayOfPointers = NULL.
+  return arrayOfPointers;
+}
+
+void print_all_tokens(char** tokens){
+  for (int i = 0; *tokens; i++){
+    printf("Tokens[%d]: %s\n", i, *tokens);
+    *tokens++;
+  }
 }
